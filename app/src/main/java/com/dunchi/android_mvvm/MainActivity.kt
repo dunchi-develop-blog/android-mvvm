e884a7d747159e2ec96e5985a1671a95356e2dbd
LDongGyu <ehdrbchd@naver.com>
2020-11-28 15:09:47 +0900

    feat: with, without AAC 추가 및 main에서 연동

--- app/src/main/java/com/dunchi/android_mvvm/MainActivity.kt
+++ app/src/main/java/com/dunchi/android_mvvm/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.dunchi.android_mvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with.setOnClickListener {
+            startActivity(Intent(this,WithAAC::class.java))
+        }
+
+        without.setOnClickListener {
+            startActivity(Intent(this,WithoutAAC::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/dunchi/android_mvvm/WithAAC.kt
@@ -0,0 +1,12 @@
+package com.dunchi.android_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WithAAC : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_with_aac)
+    }
+}
